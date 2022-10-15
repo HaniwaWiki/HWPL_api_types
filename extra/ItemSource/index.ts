8fe7283f61fb155fc15e49e6346274a65be8eb2a
lyh543 <lyh543@outlook.com>
2022-10-15 22:52:41 +0800

    feat: add ItemSource.eventAchievementRewards

--- extra/ItemSource/index.ts
+++ extra/ItemSource/index.ts
@@ -1,14 +1,16 @@
 import { GachaBox } from '@/types/HWPL/GachaBox';
+import { Event } from '@/types/HWPL/Event';
 
 // ItemSource can be fetched from /api/extra/ItemSource/<ItemId>.json
 export type ItemSource = {
-  itemId: number;
-  gachaBoxes: GachaBox[];
+  ItemId: number;
+  EventAchievementRewards: Event[];
+  GachaBoxes: GachaBox[];
 };
 
 // pre-computed aggregated information
 // ItemSourceAggregateInformation[] can be fetched from /api/extra/ItemSource/aggregateInformation.json
 export type ItemSourceAggregateInformation = {
-  itemId: number;
-  releaseTimestamp: number | null;
+  ItemId: number;
+  ReleaseTimestamp: number | null;
 };

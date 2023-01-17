6edc806596c0f37e9e08cac025055785a1c2172d
Yamamoto-2 <1810972564qq@gmail.com>
2023-01-17 21:19:44 +0900

    feat: ScenarioEventPointRankingRealtimeSummary contains full top100 information

--- ScenarioEventPointRankingRealtimeSummary.ts
+++ ScenarioEventPointRankingRealtimeSummary.ts
@@ -1,12 +1,14 @@
+import { ScenarioEventPointRanking } from './ScenarioEventPointRanking';
+
+export type ScenarioEventPointRankingReduced = Pick<
+  ScenarioEventPointRanking,
+  'PlayerId' | 'Name' | 'Rank' | 'Score'
+>;
+
 // summary of scenario event realtime point ranking
 export type ScenarioEventPointRankingRealtimeSummary = {
   Time: number;
-  Top1: number;
-  Top2: number;
-  Top3: number;
-  Top10: number;
-  Top50: number;
-  Top100: number;
+  Rankings: ScenarioEventPointRankingReduced[];
 };
 
 export type ScenarioEventPointRankingRealtimeSummarySet = {

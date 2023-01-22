5ddad2d3b78caa84d8dfdf1aab9f21c6dee2480f
Yamamoto-2 <1810972564qq@gmail.com>
2023-01-22 21:20:27 +0900

    feat: add Itemsources

--- extra/ItemSource/index.ts
+++ extra/ItemSource/index.ts
@@ -1,16 +1,47 @@
 import { GachaBox } from '../../GachaBox';
 import { Event } from '../../Event';
+import { Achievement } from '../../Achievement';
+import { Album } from '../../Album';
+import { SeasonRoad } from '../../SeasonRoad';
+import { CoupleStory } from '../../CoupleStory';
+import { TalkLive } from '../../TalkLive';
+import { PlayerLevel } from '../../PlayerLevel';
+import { MusicPart } from '../../MusicPart';
 
 // ItemSource can be fetched from /api/extra/ItemSource/<ItemId>.json
 export class ItemSource {
   ItemId: number;
   EventAchievementRewards: Event[];
   GachaBoxes: GachaBox[];
+  PlayerLevelRewards: PlayerLevel[];
+  AchievementRewards: Achievement[];
+  MusicPartAchievementThresholds: MusicPart[];
+  MusicPartLevelRewards: MusicPart[];
+  // Shop: Shop[];
+  AlbumCompleteRewards: Album[];
+  // todo: EventCharacterCardRewards: EventCharacterCard[]
+  // todo: MVEventThemePanels
+  // todo: MVEventThemePanelSets
+  SeasonRoadLevelRewards: SeasonRoad[];
+  CoupleStory: CoupleStory[];
+  EventTheater: Event[];
+  MVLibrary: Event[];
+  TalkLive: TalkLive[];
 
   constructor(ItemId = 0) {
     this.ItemId = ItemId;
     this.EventAchievementRewards = [];
     this.GachaBoxes = [];
+    this.PlayerLevelRewards = [];
+    this.AchievementRewards = [];
+    this.MusicPartAchievementThresholds = [];
+    this.MusicPartLevelRewards = [];
+    this.AlbumCompleteRewards = [];
+    this.SeasonRoadLevelRewards = [];
+    this.CoupleStory = [];
+    this.EventTheater = [];
+    this.MVLibrary = [];
+    this.TalkLive = [];
   }
 }
 
